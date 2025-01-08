6a55f8a212e51904221a9a03cfc5814e3eff46ca
quartz-developer <quartzveropix@gmail.com>
2025-01-09 00:29:40 +0330

    Finish hero component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,8 @@
-import Navbar from "@ui/layouts/navbar.jsx"; //visible on any routes & pages
+
 import '../base/_reset.scss'; //global styles
 
 export default function App({ Component, pageProps }) {
   return (<>
-    <Navbar />
     <Component {...pageProps} />
   </>)
 }
