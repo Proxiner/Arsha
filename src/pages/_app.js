9c4baa3373762f7cc936815d5fe31a6d8dbb7a40
proxiner <im.proxiner@gmail.com>
2025-01-09 11:12:18 +0330

    fixed some minor ui issues

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,5 @@
 import '../base/_reset.scss'; //global styles
-import dynamic from "next/dynamic";
-
-const Navbar = dynamic(() => import("@ui/layouts/navbar.jsx"), { ssr: false });  //visible on any routes & pages
 
 export default function App({ Component, pageProps }) {
-  return (<>
-    <Navbar />
-    <Component {...pageProps} />
-  </>)
+  return (<Component {...pageProps} />)
 }
